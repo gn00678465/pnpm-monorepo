890226dd2074b1780dd22f2e1dc13ac4141b76d7
madao <gn00678465@gmail.com>
2024-03-17 20:34:27 +0800

    feat(utility): 新增 monorepo-utility package

---
+++ packages/monorepo-utility/src/libs/utility/omitByKeys.ts
@@ -0,0 +1,37 @@
+export function omitByKeys<T extends object, K extends keyof T>(
+  obj: T,
+  propNames: ReadonlyArray<K>
+): Omit<T, K>;
+
+export function omitByKeys<T extends object, K extends keyof T>(
+  obj: T,
+  props: ReadonlyArray<K>
+): Omit<T, K> {
+  if (props.length === 0) {
+    // No props to omit at all!
+    return { ...obj };
+  }
+  if (props.length === 1) {
+    // Only one prop to omit.
+    const [propName] = props;
+    const { [propName]: _omitted, ...remaining } = obj;
+    return { ...remaining };
+  }
+  if (!props.some((prop) => prop in obj)) {
+    return { ...obj };
+  }
+  const asSet = new Set(props);
+  return Object.entries(obj)
+    .filter(([key]) => !asSet.has(key as K))
+    .reduce(
+      (obj, acc) => {
+        const [key, value] = acc as [
+          Exclude<keyof T, K>,
+          T[Exclude<keyof T, K>]
+        ];
+        obj[key as Exclude<keyof T, K>] = value;
+        return obj;
+      },
+      {} as Omit<T, K>
+    );
+}
---
+++ packages/monorepo-utility/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [dts({ rollupTypes: true })],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'main',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format}.js`
+    }
+  }
+});
