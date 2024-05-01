3592c5d3bb4b9fd3f2c25256453f94997f5719bc
Madao <gn00678465@gmail.com>
2024-05-01 10:51:54 +0800

    fix: 🐛 修正 admin-layout

---
+++ packages/materials/vue/src/admin-layout/index.ts
@@ -0,0 +1,8 @@
+// admin layout
+import AdminLayout from './index.vue';
+import type { AdminLayoutProps } from './types';
+import { LAYOUT_MAX_Z_INDEX, LAYOUT_SCROLL_ID } from './constants';
+
+// admin layout
+export { AdminLayout, LAYOUT_MAX_Z_INDEX, LAYOUT_SCROLL_ID };
+export type { AdminLayoutProps };
--- packages/materials/vue/src/index.ts
+++ packages/materials/vue/src/index.ts
@@ -1,8 +1,1 @@
-// admin layout
-import AdminLayout from './admin-layout/index.vue';
-import type { AdminLayoutProps } from './admin-layout/types';
-import { LAYOUT_MAX_Z_INDEX, LAYOUT_SCROLL_ID } from './admin-layout/constants';
-
-// admin layout
-export { AdminLayout, LAYOUT_MAX_Z_INDEX, LAYOUT_SCROLL_ID };
-export type { AdminLayoutProps };
+export * from './admin-layout';
